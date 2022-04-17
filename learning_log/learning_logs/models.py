48fa1b9fb3c4ca241be1516ba382333ff2e1d7ba
Shiv Kumar <kumarshiv_sk@outlook.com>
2022-04-17 17:12:53 -0400

    Entry model added

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -9,3 +9,13 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.text
+
+class Entry(models.Model):
+    """Learning log entries fro a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
+        
