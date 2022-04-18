c55a332c7210d8b869adeee98a9173765f1f03c3
Shiv Kumar <kumarshiv_sk@outlook.com>
2022-04-18 17:04:23 -0400

    modifications made

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 
 from .models import Topic, Entry
 
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate
+
 # Create your views here.
 def index(request):
     """The HomePage for learning_log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topic(request, topic_id):
     """Show topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
 
     context = {
